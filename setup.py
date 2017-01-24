3cdc4a4e76fa0819f3a3bef980fdc749c5522b4d
Igor Maculan <n3wtron@gmail.com>
2017-01-24 13:25:45 +0100

    fix manifest

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setup(
 	name='mssh',
-	version='0.2.0',
+	version='0.2.1',
 	packages=find_packages(),
 	url='https://github.com/foofilers/mssh',
 	license='GPLv3',

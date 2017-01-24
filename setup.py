52378d7ed6c7c5f63acdc0bb2cbbd6fc02e0e426
Igor Maculan <n3wtron@gmail.com>
2017-01-24 20:43:24 +0100

    fix compilation

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setup(
 	name='mssh',
-	version='0.3.0',
+	version='0.3.1',
 	packages=find_packages(),
 	url='https://github.com/foofilers/mssh',
 	license='GPLv3',
